f3df7dde24a37d69f479018bb6db9c95d94cbddc
erin-maxson <erin@cohatch.com>
2022-07-30 07:38:12 -0700

    separating table lines with comma

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -27,7 +27,7 @@ CREATE TABLE employee(
     role_id INT,
     manager_id INT,
     FOREIGN KEY(role_id) REFERENCES role (id)
-    ON DELETE CASCADE
+    ON DELETE CASCADE,
     FOREIGN KEY(manager_id) REFERENCES employee (id)
-    ON DELETE CASCADE
+    ON DELETE SET NULL
 );
