67f26b4334b6fecbd0e3c468a1cc17d85627e882
erin-maxson <erin@cohatch.com>
2022-07-30 07:34:29 -0700

    creating role + employee tables

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,8 +4,28 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department();
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
 
-CREATE TABLE role();
 
-CREATE TABLE employee();
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    FOREIGN KEY(department_id)
+    REFERENCES department (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY(role_id) REFERENCES role (id)
+    ON DELETE CASCADE
+);
