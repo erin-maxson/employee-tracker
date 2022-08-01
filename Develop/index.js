773766e9e63a97bda0c44957c16e1b24f58ce90d
erin-maxson <erin@cohatch.com>
2022-08-01 15:41:38 -0700

    adding menu question

--- Develop/index.js
+++ Develop/index.js
@@ -1,2 +1,21 @@
 const inquirer=require("inquirer")
 const db = require("./config/connection")
+
+db.connect( ()=> {
+    menu()
+})
+
+const menuQuestion = [
+    {
+    type:"list",
+    name:"menu",
+    message:"Choose the following options:",
+    choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"]
+    }
+]
+
+const employeeAddQuestions =
+
+function menu(){
+
+}
